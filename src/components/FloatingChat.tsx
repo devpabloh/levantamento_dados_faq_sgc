4d511f4687dad5eeb4e6788bca511f5824138a59
devpabloh <devpabloh@gmail.com>
2026-05-07 10:54:22 -0300

    resolvendo chat júlia

--- src/components/FloatingChat.tsx
+++ src/components/FloatingChat.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import ChatWidget from "./ChatWidget";
 import lisleft from "../assets/julia.png"
+import juliaMostrandoIcon from "../assets/julia_mostrar_chat.png";
+
+
 
 export default function FloatingChat() {
   const [open, setOpen] = useState(false);
@@ -9,13 +12,15 @@ export default function FloatingChat() {
     <>
       <button
         onClick={() => setOpen((v) => !v)}
-        className="fixed bottom-6 right-6 z-50 h-36 w-36 overflow-hidden rounded-full bg-indigo-600 shadow-lg hover:bg-indigo-800 hover:cursor-pointer"
+        className="fixed bottom-3 right-2 z-50 h-45 w-55 overflow-hidden hover:cursor-pointer"
         aria-label={open ? "Fechar chat" : "Abrir chat"}
         >
         {open ? (
-            <span className="flex h-full w-full items-center justify-center text-sm font-semibold text-white">
-            X
-            </span>
+            <img
+            src={juliaMostrandoIcon}
+            alt="Abrir chat"
+            className="h-full w-full object-cover"
+            />
         ) : (
             <img
             src={lisleft}
@@ -26,7 +31,7 @@ export default function FloatingChat() {
         </button>
 
       {open && (
-        <div className="fixed bottom-35 right-38 z-50 w-[92vw] max-w-md">
+        <div className="fixed bottom-35 right-40 z-50 w-[92vw] max-w-md">
           <div className="overflow-hidden rounded-2xl border border-slate-200 bg-indigo-500 shadow-2xl">
             <div className="flex items-center justify-between border-b px-4 py-3">
               <h3 className="text-sm font-semibold text-white-800 ">Assistente virtual do SGC</h3>
