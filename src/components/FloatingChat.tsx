a266ae38d25c4ed5eb8adc36b77d11bf103f136c
devpabloh <devpabloh@gmail.com>
2026-05-07 09:40:25 -0300

    Atualizando menina júlia

--- src/components/FloatingChat.tsx
+++ src/components/FloatingChat.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import ChatWidget from "./ChatWidget";
-import lisleft from "../assets/lis.png"
+import lisleft from "../assets/julia.png"
 
 export default function FloatingChat() {
   const [open, setOpen] = useState(false);
@@ -9,7 +9,7 @@ export default function FloatingChat() {
     <>
       <button
         onClick={() => setOpen((v) => !v)}
-        className="fixed bottom-6 right-6 z-50 h-28 w-28 overflow-hidden rounded-full bg-slate-900 shadow-lg hover:bg-slate-800 hover:cursor-pointer"
+        className="fixed bottom-6 right-6 z-50 h-28 w-28 overflow-hidden rounded-full bg-indigo-600 shadow-lg hover:bg-indigo-800 hover:cursor-pointer"
         aria-label={open ? "Fechar chat" : "Abrir chat"}
         >
         {open ? (
@@ -29,7 +29,7 @@ export default function FloatingChat() {
         <div className="fixed bottom-35 right-18 z-50 w-[92vw] max-w-md">
           <div className="overflow-hidden rounded-2xl border border-slate-200 bg-indigo-500 shadow-2xl">
             <div className="flex items-center justify-between border-b px-4 py-3">
-              <h3 className="text-sm font-semibold text-white-800 ">Assistente SGC</h3>
+              <h3 className="text-sm font-semibold text-white-800 ">Júlia a sua assistente virtual do SGC</h3>
               <button
                 onClick={() => setOpen(false)}
                 className="rounded-md px-2 py-1 text-xs text-white-600 hover:bg-slate-100 "
